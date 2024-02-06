14028f228401f2fc6b2459a0cf3c7afb6beb6e79
Manny <manish29polekar@gmail.com>
2024-02-06 22:00:16 +0530

    Created the places for rent and sale pages

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -4,16 +4,18 @@ import { collection, getDocs, limit, orderBy, query, startAfter, where } from "f
 import {db} from "../firebase";
 import Spinner from "../components/Spinner";
 import ListingItem from "../components/ListingItem";
+import { useParams } from 'react-router-dom';
 
-function Offers() {
+function Category() {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
   const [lastFetchedListing, setLastFetchListing] = useState(null);
+  const params = useParams()
   useEffect(() => {
     async function fetchListings() {
       try {
         const listingRef = collection(db, "listings");
-        const q = query(listingRef, where("offer", "==", true), orderBy("timestamp" , "desc"), limit(8));
+        const q = query(listingRef, where("type", "==", params.categoryName), orderBy("timestamp" , "desc"), limit(8));
         const querySnap = await getDocs(q)
         const lastVisible = querySnap.docs[querySnap.docs.length -1];
         setLastFetchListing(lastVisible);
@@ -31,11 +33,11 @@ function Offers() {
       }
     }
     fetchListings();
-  }, []);
+  }, [params.categoryName]);
   async function onFetchMoreListings() {
     try {
       const listingRef = collection(db, "listings");
-      const q = query(listingRef, where("offer", "==", true), orderBy("timestamp" , "desc"), 
+      const q = query(listingRef, where("type", "==", params.categoryName), orderBy("timestamp" , "desc"), 
       startAfter(lastFetchedListing), limit(8));
       const querySnap = await getDocs(q)
       const lastVisible = querySnap.docs[querySnap.docs.length -1];
@@ -55,7 +57,9 @@ function Offers() {
   }
   return (
     <div className="max-w-6xl mx-auto px-3">
-      <h1 className="text-3xl text-center mt-6 font-bold mb-6">Offers</h1>
+      <h1 className="text-3xl text-center mt-6 font-bold mb-6">
+        {params.categoryName === "rent" ? "Places for rent" : "Places for sale"}
+      </h1>
       {loading ? (
         <Spinner/>
       ) : listings && listings.length > 0 ? (
@@ -84,4 +88,4 @@ function Offers() {
   )
 }
 
-export default Offers
+export default Category
